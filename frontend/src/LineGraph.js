50f047aa15c61bd709c96b8f216c7d6e99e8f7b2
Jordan Lees <leesja@aston.ac.uk>
2018-11-17 22:24:01 +0000

    drawPlots can plot graph of multiple providers for a single subject

--- frontend/src/LineGraph.js
+++ frontend/src/LineGraph.js
@@ -3,7 +3,12 @@ import data from './Data'
 import Plotly from 'plotly.js-dist';
 
 
-var traceSingleSubSexInstitution = function(provider, sex, subject, name) {
+var traceSingleLine = function(provider, sex, subject, name) {
+    
+    if (!name) {
+        name = "";
+    }
+
     let list = data();
 
     var xVals = list[provider][sex][subject].years;
@@ -21,11 +26,29 @@ var traceSingleSubSexInstitution = function(provider, sex, subject, name) {
 }
 
 var drawPlot = function() {
-    let dataM = traceSingleSubSexInstitution("Imperial College of Science Technology and Medicine", "Male", "Computer science", "Males");
-    let dataF = traceSingleSubSexInstitution("Imperial College of Science Technology and Medicine", "Female", "Computer science", "Females");
-    let dataB = traceSingleSubSexInstitution("Imperial College of Science Technology and Medicine", "Both", "Computer science", "Average");
+    let dataM = traceSingleLine("Imperial College of Science Technology and Medicine", "Male", "Computer science", "Males");
+    let dataF = traceSingleLine("Imperial College of Science Technology and Medicine", "Female", "Computer science", "Females");
+    let dataB = traceSingleLine("Imperial College of Science Technology and Medicine", "Both", "Computer science", "Average");
     console.log(data);
     Plotly.plot('plotme', [dataM, dataF, dataB]);
 }
 
-export default drawPlot;
+var drawMyPlot = function() {
+    drawPlots(["Imperial College of Science Technology and Medicine", "Aston University"], "Computer science");
+}
+
+// draw multiple plots: multiple providers, single subject. default sex average
+var drawPlots = function(providers, subject) {
+    let traces = [];
+    let layout = {
+        title: "Average earnings by years after graduation"
+    }
+
+    providers.forEach(function(provider) {
+        traces.push(traceSingleLine(provider, "Both", subject, provider))
+    });
+
+    Plotly.plot('plotme', traces, layout);
+}
+
+export default drawMyPlot;
