840e9ed73924436fc6c47c1764a02b4b23755367
jlp-io <jlp3@illinois.edu>
2018-11-17 14:06:42 +0000

    added welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Typeahead } from 'react-bootstrap-typeahead'
+import axios from 'axios'
+import { Grid, PageHeader, Form, Row, Col } from 'react-bootstrap'
+import { withRouter } from 'react-router'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+	<header className="App-header">
+	<div className="page-body-grey">
+        <Grid
+          fluid
+          className="query-form-center padded-element-shrink round-corners-large solid-background"
+        >
+          <Row>
+            <Col sm={12} md={12} className="bs-center">
+              <PageHeader className="page-header-montserrat">
+				Longitudinal Education Outcomes: Data Mining and Visualization
+              </PageHeader>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12} md={12}>
+              <Form>
+                <p style={{textAlign: "center"}}>
+					Search courses
+                </p>
+              </Form>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={6} sm={6} md={6}>
+            </Col>
+            <Col xs={6} sm={6} md={6} className="bs-button-right">
+            </Col>
+          </Row>
+        </Grid>
       </div>
-    );
+	  </header>
+	  );
   }
 }
 
